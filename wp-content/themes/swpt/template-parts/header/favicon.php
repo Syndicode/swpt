3ac53770003e4880368cb60e289210ac636895e7
Synianskyi Tymofii <janosense@gmail.com>
2025-03-10 18:41:52 +0200

    [routine] Changed Favicon

--- wp-content/themes/swpt/template-parts/header/favicon.php
+++ wp-content/themes/swpt/template-parts/header/favicon.php
@@ -1,8 +1,11 @@
-<link rel="icon" type="image/png" href="/wp-content/themes/swpt/source/images/favicon/favicon-96x96.png?v=1.0.2"
+<?php
+$favicon_version = '1.0.4';
+?>
+<link rel="icon" type="image/png" href="/wp-content/themes/swpt/source/images/favicon/favicon-96x96.png?v=<?= $favicon_version; ?>"
       sizes="96x96"/>
-<link rel="icon" type="image/svg+xml" href="/wp-content/themes/swpt/source/images/favicon/favicon.svg?v=1.0.2"/>
-<link rel="shortcut icon" href="/wp-content/themes/swpt/source/images/favicon/favicon.ico?v=1.0.2"/>
+<link rel="icon" type="image/svg+xml" href="/wp-content/themes/swpt/source/images/favicon/favicon.svg?v=<?= $favicon_version; ?>"/>
+<link rel="shortcut icon" href="/wp-content/themes/swpt/source/images/favicon/favicon.ico?v=<?= $favicon_version; ?>"/>
 <link rel="apple-touch-icon" sizes="180x180"
-      href="/wp-content/themes/swpt/source/images/favicon/apple-touch-icon.png?v=1.0.2"/>
+      href="/wp-content/themes/swpt/source/images/favicon/apple-touch-icon.png?v=<?= $favicon_version; ?>"/>
 <meta name="apple-mobile-web-app-title" content="MyWebSite"/>
-<link rel="manifest" href="/wp-content/themes/swpt/source/images/favicon/site.webmanifest?v=1.0.2"/>
+<link rel="manifest" href="/wp-content/themes/swpt/source/images/favicon/site.webmanifest?v=<?= $favicon_version; ?>"/>
